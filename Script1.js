7776214020a16d9fe7b748ce912ef4727a96fdcc
kishankm19 <94464104+kishankm19@users.noreply.github.com>
2022-11-05 01:55:31 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,7 +1,7 @@
 <!-- h2{
 <!--background-color: #FEFFED;
-padding: 30px 35px;
-margin: -10px -50px;
+<!--padding: 30px 35px;
+/*margin: -10px -50px;
 text-align:center;
 border-radius: 10px 10px 0 0;
 }
@@ -57,7 +57,212 @@ text-decoration: none;
 border: 1px solid rgb(0, 143, 255);
 background-color: rgb(0, 214, 255);
 padding: 3px 20px;
+bordpadding: 3px 20px;
 border-radius: 2px;
+color: black;
+}
+input[type=button]{
+font-size: 16px;
+background: linear-gradient(#ffbc00 5%, #ffdd7f 100%);
+border: 1px solid #e5a900;
+color: #4E4D4B;
+font-weight: bold;
+cursor: pointer;
+width: 100%;
+border-radius: 5px;
+padding: 10px 0;
+outline:none;
+}
+input[type=button]:hover{
+background: linear-gradient(#ffdd7f 5%, #ffbc00 100%);
+}
+Copy
+Pabbly Form Builder
+
+Conclusion:
+
+Hence, we have applied JavaScript validation on login form, you can also use database to verify user. Hope you like it, keep reading our other blogs in future.
+
+Related Posts:
+JavaScript Onsubmit Event with form validation
+JavaScript Change Form Action Dynamically
+JavaScript Serialize Form Data
+Prev
+JavaScript Change Form Action Dynamically
+JavaScript Onsubmit Event with form validation
+Next
+23 Replies to “JavaScript Form Validation With Limit Login Attempts”
+muhammad
+NOVEMBER 1, 2014 AT 5:25 PM
+new style login example coding
+
+REPLY
+muhammad
+NOVEMBER 1, 2014 AT 5:26 PM
+new style login example coding
+
+REPLY
+mital
+DECEMBER 29, 2014 AT 9:45 AM
+this code is wrong
+
+REPLY
+admin
+DECEMBER 29, 2014 AT 10:07 AM
+Hello Mital,
+
+Could you please let me know that where do you find the mistake in the code so that we can make it correct.
+
+Thanks & Regards,
+Swapnil Gupta,
+FormGet.com
+
+REPLY
+himanshu
+AUGUST 31, 2018 AT 7:04 PM
+No, this code is right.
+
+REPLY
+alishba
+JANUARY 1, 2015 AT 7:39 AM
+the code is OK
+
+REPLY
+Ashish
+JANUARY 8, 2015 AT 6:09 AM
+Thanks for this code 🙂
+
+but
+Can u help me?
+how to log in using web services ???
+
+REPLY
+Dhanush
+JANUARY 13, 2015 AT 2:22 AM
+if we want to use many user id and password what can we do?
+
+REPLY
+Jilla
+APRIL 6, 2016 AT 5:52 AM
+We can save those values in database then only it will be possible
+
+REPLY
+Nandan
+FEBRUARY 2, 2015 AT 3:24 PM
+I want the code for forget password
+
+REPLY
+rogelio
+FEBRUARY 14, 2015 AT 8:16 AM
+you have a very nice code
+
+REPLY
+Jake Anderson
+MARCH 3, 2015 AT 5:35 PM
+Hey is there one where you type in the login and password and then it sends you to chosen link.
+
+REPLY
+Datuk Seri Najib
+MARCH 17, 2015 AT 5:42 AM
+I have copy all ur code and teste it,the javascript doesnt show any error.This code is for HTML 4 or HTML 5?
+
+REPLY
+Firan
+MARCH 30, 2015 AT 6:09 PM
+nice code
+
+REPLY
+sheetal
+JULY 8, 2015 AT 10:12 AM
+i need the .project file of this code
+
+REPLY
+Speed Subash
+JULY 23, 2015 AT 6:18 AM
+Ur code is awesome !!! Can u please tell me how to merge the same with my java game and run it using Eclipse. Is it possible to do it. Please help me i am new to this field ………………………
+
+REPLY
+ekky
+JULY 29, 2015 AT 11:18 AM
+Very simple and effective… Thanks!
+But i have a follow-up question. How should we code success.html, such that it will refuse to show, if the user skips javascript_login.html, and goes directly to success.html?
+
+In other words, how do we authenticate if the user browsing success.html has actually provided authenticated, or even authorized credentials? Is there a simple way to do this? I find Owin and stuff very complicated.
+
+REPLY
+Vishal
+SEPTEMBER 11, 2015 AT 11:30 AM
+Hi,
+I am trying to get this login form with more then 1 user but cannot get through. can you please help me on this ,
+
+Thanks,
+
+REPLY
+mwangi
+NOVEMBER 16, 2015 AT 5:46 PM
+this code is awesome
+
+REPLY
+Fabian
+DECEMBER 1, 2015 AT 7:12 AM
+Thank you it’s exactly that i needed, very useful
+
+REPLY
+Website Designing
+DECEMBER 19, 2015 AT 5:22 AM
+Hi there, yeah this article is really pleasant and I have learned lot
+of things from it about blogging. thanks.
+
+REPLY
+Faizan
+JANUARY 11, 2016 AT 8:39 AM
+Thank u so must dude
+
+REPLY
+Aljghon
+JANUARY 22, 2016 AT 5:16 AM
+i’m just trying to creat a simple login and logout form using javascript only.
+can you teach me how to do it?
+
+REPLY
+Leave a Reply
+Your email address will not be published. Required fields are marked *
+
+Comment *
+
+Name *
+
+Email *
+
+Website
+
+
+Take Your Email Marketing To The Next Level!
+Start using MailGet and experience world class email builder, create email newsletters in a snap and send great
+emails to your audience. Do amazing email marketing at an affordable price.
+TRY IT FOR FREE
+Our Solution
+Subscription Billing
+Form Builder
+Email Marketing
+Email List Cleaning
+Connect With Us
+Contact Us
+Careers
+Company
+About Us
+Privacy Policy
+Terms and Conditions
+Affiliate
+Branding
+Resources
+Blog
+Blog Categories
+FormGet Features
+Email Templates
+Form Templates
+JavaScript
+Analyticser-radius: 2px;
 color: black;
 }
 input[type=button]{
@@ -264,8 +469,7 @@ JavaScript
 Analytics
 JavaScript
 FormGet © 2021. All rights reserved.
-
--->
+*/
 function validate(){
 var password = document.getElementById("password").value;
 var username = document.getElementById("username").value;
