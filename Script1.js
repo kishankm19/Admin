72ee434a7019fd969aac64210eed26a727c07526
kishankm19 <94464104+kishankm19@users.noreply.github.com>
2022-11-04 20:54:56 +0530

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,12 @@
+function validate(){
+var password = document.getElementById("password").value;
+var username = document.getElementById("username").value;
+if(username="admin" && password == "revactf201")
+   {
+   window.location = "ctf.html";
+   }
+else
+{
+  alert("🤡I was expecting you would type the wrong credentials!!");
+}
+}
